5179d9414e83394d7cfec6a96cfe3818c766a1d2
awwa500@gmail.com <awwa500@gmail.com>
2022-05-16 00:39:16 +0900

    Add Cars.razor to Client

--- Client/Pages/Cars.razor.cs
+++ Client/Pages/Cars.razor.cs
@@ -1,11 +1,21 @@
+using System.Reflection;
 using HogeBlazor.Client.Helpers;
 using HogeBlazor.Client.Repositories;
+// using HogeBlazor.Shared.Models;
 using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
 
 namespace HogeBlazor.Client.Pages;
 public partial class Cars
 {
     public List<Car> CarList { get; set; } = new List<Car>();
+
+    public string TitleMakerName { get; set; } = CommentHelper.GetCommentAttribute<HogeBlazor.Shared.Models.Car>("MakerName");
+    public string TitleModelName { get; set; } = CommentHelper.GetCommentAttribute<HogeBlazor.Shared.Models.Car>("ModelName");
+    public string TitleGradeName { get; set; } = CommentHelper.GetCommentAttribute<HogeBlazor.Shared.Models.Car>("GradeName");
+    public string TitleModelCode { get; set; } = CommentHelper.GetCommentAttribute<HogeBlazor.Shared.Models.Car>("ModelCode");
+    public string TitlePrice { get; set; } = CommentHelper.GetCommentAttribute<HogeBlazor.Shared.Models.Car>("Price");
+    public string TitleModelChangeFull { get; set; } = CommentHelper.GetCommentAttribute<HogeBlazor.Shared.Models.Car>("ModelChangeFull");
 
     [Inject]
     public ICarHttpRepository CarRepo { get; set; } = default!;
