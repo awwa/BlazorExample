28b3a5ae28b823bb62f6172235b0848d12514f7b
awwa500@gmail.com <awwa500@gmail.com>
2022-12-28 14:13:39 +0900

    Add EngineSpecItem

---
+++ Client/Shared/EngineSpecItem.razor.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Components;
+
+namespace HogeBlazor.Client.Shared;
+public partial class EngineSpecItem
+{
+    [Parameter]
+    public string FcrWltcTitle { get; set; } = string.Empty;
+
+    [Parameter]
+    public string FcrWltc { get; set; } = string.Empty;
+
+    [Parameter]
+    public string FcrWltcLTitle { get; set; } = string.Empty;
+
+    [Parameter]
+    public string FcrWltcL { get; set; } = string.Empty;
+
+    [Parameter]
+    public string FcrWltcMTitle { get; set; } = string.Empty;
+
+    [Parameter]
+    public string FcrWltcM { get; set; } = string.Empty;
+
+    [Parameter]
+    public string FcrWltcHTitle { get; set; } = string.Empty;
+
+    [Parameter]
+    public string FcrWltcH { get; set; } = string.Empty;
+
+    [Parameter]
+    public string MpcWltcTitle { get; set; } = string.Empty;
+
+    [Parameter]
+    public string MpcWltc { get; set; } = string.Empty;
+}
