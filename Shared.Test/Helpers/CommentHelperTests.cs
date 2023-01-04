47a9aee01665ce21046f5504afe0a96f1680dfe7
awwa500@gmail.com <awwa500@gmail.com>
2023-01-04 18:42:16 +0900

    Cars Page調整

--- Shared.Test/Helpers/CommentHelperTests.cs
+++ Shared.Test/Helpers/CommentHelperTests.cs
@@ -8,35 +8,35 @@ namespace HogeBlazor.Shared.Test.Helpers;
 
 public class CommentHelperTests
 {
-    [Fact]
-    public void GetCommentAttributeReturnsValidComment()
-    {
-        // Arrange
-        // Act
-        var actual = CommentHelper.GetCommentAttributeOnProperty<Car>("ModelName");
-        // Assert
-        Assert.Equal("モデル名", actual);
-    }
+    // [Fact]
+    // public void GetCommentAttributeReturnsValidComment()
+    // {
+    //     // Arrange
+    //     // Act
+    //     var actual = CommentHelper.GetCommentAttributeOnProperty<Car>("ModelName");
+    //     // Assert
+    //     Assert.Equal("モデル名", actual);
+    // }
 
-    [Fact]
-    public void GetCommentAttributeReturnsValidValueForOwnedClassProperty()
-    {
-        // Arrange
-        // Act
-        var actual = CommentHelper.GetCommentAttributeOnProperty<Body>("Type");
-        // Assert
-        Assert.Equal("ボディタイプ", actual);
-    }
+    // [Fact]
+    // public void GetCommentAttributeReturnsValidValueForOwnedClassProperty()
+    // {
+    //     // Arrange
+    //     // Act
+    //     var actual = CommentHelper.GetCommentAttributeOnProperty<Body>("Type");
+    //     // Assert
+    //     Assert.Equal("ボディタイプ", actual);
+    // }
 
-    [Fact]
-    public void GetCommentAttributeReturnsEmptyValue()
-    {
-        // Arrange
-        // Act
-        // Assert
-        Assert.Throws<NotImplementedException>(() =>
-        {
-            CommentHelper.GetCommentAttributeOnProperty<Car>("存在しないプロパティ");
-        });
-    }
+    // [Fact]
+    // public void GetCommentAttributeReturnsEmptyValue()
+    // {
+    //     // Arrange
+    //     // Act
+    //     // Assert
+    //     Assert.Throws<NotImplementedException>(() =>
+    //     {
+    //         CommentHelper.GetCommentAttributeOnProperty<Car>("存在しないプロパティ");
+    //     });
+    // }
 }
--- Shared/Helpers/CommentHelper.cs
+++ Shared/Helpers/CommentHelper.cs
@@ -5,56 +5,6 @@ using Microsoft.EntityFrameworkCore;
 public static class CommentHelper
 {
     /// <summary>
-    /// プロパティのCommentAttributeの値を返す
-    /// </summary>
-    /// <typeparam name="T">対象クラス</typeparam>
-    /// <param name="property">プロパティ名</param>
-    /// <returns>CommentAttributeの値</returns>
-    /// <exception cref="NotImplementedException"></exception>
-    public static string GetCommentAttributeOnProperty<T>(string property)
-    {
-        if (property == null)
-        {
-            return "";
-        }
-        Type t = typeof(T);
-        MemberInfo? pi = t.GetProperty(property);
-        // nullの場合、プロパティが存在しないエラー
-        if (pi == null) throw new NotImplementedException();
-        // 最初のCommentAttributeを取得
-        Attribute comment = Attribute.GetCustomAttributes(pi, typeof(CommentAttribute)).First();
-        CommentAttribute? c = comment as CommentAttribute;
-        if (c is not null)
-        {
-            return c.Comment;
-        }
-        else
-        {
-            return "";
-        }
-    }
-
-    public static string GetCommentOnProperty(string className, string property)
-    {
-        Type? t = Type.GetType(className);
-        if (t == null) throw new NotImplementedException();
-        MemberInfo? pi = t.GetProperty(property);
-        // nullの場合、プロパティが存在しないエラー
-        if (pi == null) throw new NotImplementedException();
-        // 最初のCommentAttributeを取得
-        Attribute comment = Attribute.GetCustomAttributes(pi, typeof(CommentAttribute)).First();
-        CommentAttribute? c = comment as CommentAttribute;
-        if (c is not null)
-        {
-            return c.Comment;
-        }
-        else
-        {
-            return "";
-        }
-    }
-
-    /// <summary>
     /// constフィールドのCommentAttributeの値を返す
     /// </summary>
     /// <typeparam name="T">対象クラス</typeparam>
